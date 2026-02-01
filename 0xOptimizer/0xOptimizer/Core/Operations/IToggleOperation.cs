5956748c9f8d2b3c97d1b95407956aee319afe84
berperly <135066562+berperly@users.noreply.github.com>
2026-01-31 22:02:54 -0300

    Delete 0xOptimizer directory
    shit

--- 0xOptimizer/0xOptimizer/Core/Operations/IToggleOperation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _0xOptimizer.Core.Operations
-{
-    public interface IToggleOperation : IOperation
-    {
-        bool DefaultEnabled { get; }
-        void Set(IOperationContext ctx, bool enabled);
-        bool? IsEnabled();
-    }
-}
